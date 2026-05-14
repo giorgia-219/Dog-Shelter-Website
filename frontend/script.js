d02f3062e85408bd972c9ed759e3803f22aedbf0
gmanocchio <g.manocchio@unibz.it>
2026-05-14 19:34:43 +0200

    Added inquiry form on contact us page

---
+++ frontend/script.js
@@ -0,0 +1,6 @@
+/* add logic for form:
+
+1. input validation
+2. hide "Do you currently have pets?" and "Which pets have you had before?" for first time owners
+3. hide "Which pets do you currently have?" for users who don't currently have pets
+4. add "other" text field that appears when other is clicked */
